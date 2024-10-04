7e73b8f9f94d588469a614fe46f4a22939e9f974
dhivyasreekr <dhivyasreekr@gmail.com>
2024-10-04 13:31:28 +0530

    Update AppLayout.jsx

--- src/components/frontend/layout/AppLayout.jsx
+++ src/components/frontend/layout/AppLayout.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import Navbar from '../Navbar';
-import Header from '../Header';
-import Footer from '../Footer';
+import React, { useState, useEffect } from 'react';
+import TopBar from './TopBar';
+import Header from './Header';
+import Footer from './Footer';
+import { useAuth } from '../../../context/AuthContext'; // Import the useAuth hook
 
-const AppLayout = ({ children }) => {
+const AppLayout = ({ children }) => { 
+  // Get the user and authentication state from AuthContext
+  const { user } = useAuth();
+    
+  // Local state for authentication status
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  // useEffect hook to update the authentication state when the user changes
+  useEffect(() => {
+
+    console.log("use effect working");
+    console.log(!!user);
+    
+    // Check if user exists and update the isAuthenticated state
+
+    setIsAuthenticated(!!user); // Convert user to boolean, true if exists, false otherwise
+
+  }, [user]); // This hook will run whenever the `user` value changes
+
+
+  
   return (
     <div>
-      <Navbar />
+      <TopBar isAuthenticated={isAuthenticated} user={user}/>
       <Header />
       <main>{children}</main>
       <Footer />
